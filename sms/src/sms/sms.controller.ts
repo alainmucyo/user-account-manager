670f1475789733925664c47389a94a40e4f13932
Alain MUCYO <alainmucyo3@gmail.com>
2022-08-25 11:32:59 +0200

    Format codes

--- sms/src/sms/sms.controller.ts
+++ sms/src/sms/sms.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from "@nestjs/common";
 import { MessagePattern, Payload } from "@nestjs/microservices";
 import { SmsDto } from "./dto/sms.dto";
 import { SmsService } from "./sms.service";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
 
 @Controller()
@@ -9,8 +10,8 @@ export class SmsController {
   constructor(private readonly smsService: SmsService) {}
   @MessagePattern(process.env.SEND_SMS_REQUEST_TOPIC)
   handleSMSRequest(@Payload() payload: any) {
-    const value: SmsDto=payload.value;
+    const value: SmsDto = payload.value;
     console.log("Received SMS request: ", payload);
-    this.smsService.sendSMS(payload.value);
+    this.smsService.sendSMS(value);
   }
 }
